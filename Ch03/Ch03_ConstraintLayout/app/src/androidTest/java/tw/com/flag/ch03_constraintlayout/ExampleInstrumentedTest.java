ddcf58868298b13315a1659a95c72de965d947c3
zero2005x <zero2005x@gmail.com>
2025-11-03 23:16:56 +0800

    Update dependencies and fix XML namespace for ConstraintLayout

--- Ch03/Ch03_ConstraintLayout/app/src/androidTest/java/tw/com/flag/ch03_constraintlayout/ExampleInstrumentedTest.java
+++ Ch03/Ch03_ConstraintLayout/app/src/androidTest/java/tw/com/flag/ch03_constraintlayout/ExampleInstrumentedTest.java
@@ -19,7 +19,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("tw.com.flag.ch03_constraintlayout", appContext.getPackageName());
     }
